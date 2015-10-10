25d796885d11b48d6bac0846a0a1079ebc28a2c1
tomasilucas@gmail.com <tomasilucas@gmail.com>
2015-10-10 19:23:05 -0300

    asd
    asd

---
+++ AerolineaFrba/Abm Ciudad/ABMCiudadBajaModif.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AerolineaFrba.CapaADO;
+
+namespace AerolineaFrba.Abm_Ciudad
+{
+    public partial class ABMCiudadBajaModif : Form
+    {
+        public ABMCiudadBajaModif()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFiltroNumero_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataListadoCiudades_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dataListadoCiudades.Columns["dataGridViewCheckBoxColumn1"].Index)
+            {
+                DataGridViewCheckBoxCell ChkEliminar = (DataGridViewCheckBoxCell)dataListadoCiudades.Rows[e.RowIndex].Cells["dataGridViewCheckBoxColumn1"];
+                ChkEliminar.Value = !Convert.ToBoolean(ChkEliminar.Value);
+            }
+        }
+
+        private void Mostrar()
+        {
+            this.dataListadoCiudades.DataSource = DAOCiudad.Mostrar();
+            this.OcultarColumnas();
+            lblTotal.Text = "Total de Registros: " + Convert.ToString(dataListadoAeronaves.Rows.Count);
+        }
+    }
+}
