2bf2b0f7c9639d4fdffad6f427b01c0eb82bbe49
tomasilucas@gmail.com <tomasilucas@gmail.com>
2015-10-10 18:34:09 -0300

    sd
    sdf

---
+++ AerolineaFrba/ConstructorDeClases/Ciudad.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AerolineaFrba.ConstructorDeClases
+{
+
+    public class Ciudad
+    {
+        private int _id_ciudad;
+        private string _nombre;
+        private string _pais;
+
+        public Ciudad(int codigoCiudad, string nombre,string  pais) 
+        {
+            IdentificadorCiudad = codigoCiudad;
+            Nombre =  nombre;
+            Pais = pais;
+
+        }
+
+        public int IdentificadorCiudad
+        {
+            get { return _id_ciudad; }
+            set { _id_ciudad = value; }
+        }
+
+        
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        public string Pais
+        {
+            get { return _pais; }
+            set { _pais = value; }
+        }
+    }
+}
