bca5f009a96014b1f635abf9d46fb7f9d64115c2
lucastomasi <tomasilucas@gmail.com>
2015-10-10 17:57:09 -0300

    ciudad abm

---
+++ AerolineaFrba/Abm Ciudad/ABMCiudadAlta.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AerolineaFrba.CapaADO;
+using AerolineaFrba.ConstructorDeClases;
+
+namespace AerolineaFrba.Abm_Ciudad
+{
+    public partial class ABMCiudadAlta : Form
+    {
+        public ABMCiudadAlta()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+         
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+
+            
+            try
+            {
+
+              
+                DAOCiudad.AgregarCiudad(CargarCiudad());
+                MessageBox.Show("La Ciudad se agregó correctamente.");
+                limpiar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hubo un error." + ex.Message);
+            }
+        }
+
+       
+        
+        private Ciudad CargarCiudad() 
+        {
+
+            return new Ciudad(1, textNombre.Text, textPais.Text);
+                                 
+        }
+
+        private void limpiar()
+        {
+            textPais.Text = "";
+            textNombre.Text = "";
+
+        }
+
+        private void textNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            limpiar();
+        }
+
+       
+    }
+
+
+      
+}
