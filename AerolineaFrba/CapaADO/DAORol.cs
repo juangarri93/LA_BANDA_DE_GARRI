1e4726bf5bb836254ddd506faeb06ac5d7f24451
Messa Nicolas <nnico3691@gmail.com>
2015-10-11 12:02:46 -0300

    Subo Parte de rol y cree clase persona

---
+++ AerolineaFrba/CapaADO/DAORol.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AerolineaFrba.CapaADO
+{
+    class DAORol
+    {
+        public static void AgregarRol(object p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AerolineaFrba/ConstructorDeClases/Rol.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AerolineaFrba.ConstructorDeClases
+{
+    public class Rol
+    {
+
+        private int _Codigo;
+        private string _Nombre;
+        private bool _Habilitado;
+        private List<int> _Funcionalidades;
+
+        public int Codigo
+        {
+            get { return _Codigo; }
+            set { _Codigo = value; }
+        }
+
+        public string Nombre
+        {
+            get { return _Nombre; }
+            set { _Nombre = value; }
+        }
+
+        public bool Habilitado
+        {
+            get { return _Habilitado; }
+            set { _Habilitado = value; }
+        }
+
+        public List<int> Funcionalidades
+        {
+            get { return _Funcionalidades; }
+            set { _Funcionalidades = value; }
+        }
+
+
+        public Rol(string nombre, List<int> funcionalidades) 
+        {
+        
+        
+        
+        }
+
+
+
+    }
+}
