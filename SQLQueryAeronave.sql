20c87a8d6bd0f282d112a62ae5a88abe5c353573
Messa Nicolas <nnico3691@gmail.com>
2015-10-13 19:42:14 -0300

    EL script que usamos para probar se puede ejecutar entero

--- SQLQueryAeronave.sql
+++ SQLQueryAeronave.sql
@@ -1,4 +1,101 @@
-CREAR TABLA AERONAVE--
+-----------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
+---------------- ELIMINAR Y DROPEAR TABLAS Y PROCEDIMIENTOS ----------------------
+----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+--ELIMINAR TABLA rol_funcionalidad--
+drop table rol_funcionalidad
+go
+
+--------------------------------------------------------------------------------
+
+--ELIMINAR TABLA ROL--
+drop table Rol;
+go
+
+
+-------------------------------------------------------------------------------------
+
+--ELIMINAR TABLA FUNCIONALIDAD--
+drop table Funcionalidad;
+go
+
+-----------------------------------------------------------------------------------
+
+--ELIMINAR TABLA AERONAVE--
+drop table Aeronave
+go
+
+------------------------------------------------------------------------------------
+
+--ELIMINAR TABLA CIUDAD--
+drop table Ciudad
+go
+
+----------------------------------------------------------------------------------------
+drop proc speliminar_ciudad
+go
+
+drop proc spinsertar_ciudad
+go
+
+drop proc spbuscarModelo_aeronave
+go
+
+drop proc speditar_ciudad
+go
+
+drop proc spmostrar_ciudad
+go
+
+drop proc spMostrar_Funcionalidades
+
+--ELIMINAR insertar rol-funcionalidad en tabla rol_funcionalidad--
+drop proc spinsertar_rol_funcionalidad
+go
+
+
+--ELIMINAR PROCEDIMIENTO spinsertar_rol--
+DROP PROC spinsertar_rol;
+go
+
+--ELIMINAR PROCEDIMIENTO INSERTAR--
+drop proc spinsertar_aeronave;
+go
+
+--ELIMINAR PROCEDIMIENTO MOSTRAR--
+drop proc spmostrar_aeronave;
+go
+
+--ELIMINAR PROCEDIMIENTO BUSCAR NUMERO--
+drop proc spbuscarNombre_aeronave;
+go
+
+--ELIMINAR PROCEDIMIENTO BUSCAR FABRICANTE--
+drop proc spbuscarFabricante_aeronave;
+go
+
+--ELIMINAR PROCEDIMIENTO speditar_aeronave --
+DROP PROC speditar_aeronave
+go
+
+--ELIMINAR PROCEDIMIENTO speliminar_aeronave--
+DROP PROC speliminar_aeronave
+go
+
+--ELIMINAR PROCEDIMIENTO spdardebaja_aerolinea--
+DROP PROC spdardebaja_aerolinea
+go
+
+----------------------------------------------
+----------------------------------------------
+---------------- TABLAS ----------------------
+----------------------------------------------
+----------------------------------------------
+
+--CREAR TABLA AERONAVE--
 CREATE TABLE dbo.Aeronave
    (CodigoAeronave int PRIMARY KEY NOT NULL identity ,
     FechaAlta datetime NOT NULL,
@@ -16,6 +113,52 @@ CREATE TABLE dbo.Aeronave
 	KgDisponible int NOT NULL,
 	)
 go
+
+-------------------------------------------------
+
+--CREAR TABLA Ciudad--
+CREATE TABLE dbo.Ciudad
+   (CodigoCiudad int PRIMARY KEY NOT NULL identity ,
+    Nombre varchar(50) NOT NULL,
+	Pais varchar(50) NOT NULL, 
+	)
+go
+
+--------------------------------------------------
+
+--PROCEDIMIENTO CREAR TABLA Funcionalidad--
+create table Funcionalidad
+(id_funcionalidad int PRIMARY KEY not null identity,
+nombre varchar(100)
+)
+go
+
+---------------------------------------------------
+
+--PROCEDIMIENTO CREAR TABLA ROL --
+CREATE TABLE Rol
+(id_rol int PRIMARY KEY not null identity,
+nombre varchar(100),
+habilitado binary
+)
+go
+
+------------------------------------------------------
+
+--CREAR TABLA rol_funcionalidad--
+CREATE TABLE dbo.rol_funcionalidad
+(id_rol int foreign key references Rol(id_rol),	
+id_funcionalidad int foreign key references Funcionalidad(id_funcionalidad)
+)
+go
+
+-------------------------------------------------------
+
+-------------------------------------------------------
+-------------------------------------------------------
+---------------- PROCEDIMIENTOS  ----------------------
+-------------------------------------------------------
+-------------------------------------------------------
 
 --PROCEDIMIENTO INSERTAR AERONAVE EN TABLA AERONAVE--
 create proc spinsertar_aeronave
@@ -36,6 +179,8 @@ insert into Aeronave(FechaAlta,NumeroAeronave,Modelo,Matricula,Fabricante,TipoDe
 values(@fechaAlta,@numeroAeronave,@modelo,@matricula, @fabricante,@tipoDeServicio, @bajaPorFueraDeServicio,'Habilitado',@FechaDeFueraDeServicio,@FechaDeReinicioDeServicio,'2050-01-01' ,@CantidadButacas, @kgDisponible)
 go
 
+------------------------------------------------------------
+
 --PROCEDIMIENTO BUSCAR NUMERO--
 create proc spbuscarNombre_aeronave
 @textoBuscar varchar(50)
@@ -43,6 +188,8 @@ as
 select * from Aeronave
 where NumeroAeronave like @textoBuscar + '%'
 go
+
+--------------------------------------------------------------
 
 --PROCEDIMIENTO BUSCAR FABRICANTE--
 create proc spbuscarFabricante_aeronave
@@ -52,6 +199,8 @@ select * from Aeronave
 where Fabricante like @textoBuscar + '%'
 go
 
+---------------------------------------------------------------
+
 --PROCEDIMIENTO BUSCAR MODELO--
 create proc spbuscarModelo_aeronave
 @textoBuscar varchar(50)
@@ -59,6 +208,8 @@ as
 select * from Aeronave
 where Modelo like @textoBuscar + '%'
 go
+
+-----------------------------------------------------------------
 
 --PROCEDIMIENTO EDITAR AERONAVE--
 create proc speditar_aeronave
@@ -93,12 +244,16 @@ KgDisponible = @kgDisponible
 where CodigoAeronave = @codigo
 go
 
+----------------------------------------------------------
+
 --PROCEDIMIENTO MOSTRAR TABLA AERONAVE--
 create proc spmostrar_aeronave
 as
 select * from Aeronave
 order by CodigoAeronave
 go
+
+------------------------------------------------------------
 
 --PROCEDIMIENTO ELIMINAR AERONAVE--
 create proc speliminar_aeronave
@@ -108,6 +263,8 @@ delete from Aeronave
 where CodigoAeronave = @codigoAeronave
 go
 
+------------------------------------------------------------
+
 --PROCEDIMIENTO spdardebaja_aerolinea --
 create proc spdardebaja_aerolinea
 @CodigoAeronave int
@@ -116,13 +273,7 @@ update Aeronave Set BajaPorVidaUtil = 'Deshabilitado',FechaDeBajaDefinitiva = ge
 where @CodigoAeronave = CodigoAeronave
 go
 
---CREAR TABLA Ciudad--
-CREATE TABLE dbo.Ciudad
-   (CodigoCiudad int PRIMARY KEY NOT NULL identity ,
-    Nombre varchar(50) NOT NULL,
-	Pais varchar(50) NOT NULL, 
-	)
-go
+-------------------------------------------------------------
 
 --PROCEDIMIENTO INSERTAR ciudad EN TABLA Ciudad--
 create proc spinsertar_ciudad
@@ -133,6 +284,8 @@ as
 insert into Ciudad(Nombre,Pais)
 values(@nombre, @pais)
 go
+
+----------------------------------------------------------------
 
 --PROCEDIMIENTO EDITAR Ciudad--
 create proc speditar_ciudad
@@ -145,12 +298,16 @@ Pais = @pais
 where CodigoCiudad = @codigo
 go
 
+-----------------------------------------------------------------------
+
 --PROCEDIMIENTO MOSTRAR TABLA Ciudad--
 create proc spmostrar_ciudad
 as
 select * from Ciudad
 order by CodigoCiudad
 go
+
+--------------------------------------------------------------------------
 
 --PROCEDIMIENTO ELIMINAR CIUDAD--
 create proc speliminar_ciudad
@@ -160,12 +317,7 @@ delete from Ciudad
 where CodigoCiudad = @codigoCiudad
 go
 
---PROCEDIMIENTO CREAR TABLA Funcionalidad--
-create table Funcionalidad
-(id_funcionalidad int PRIMARY KEY not null identity,
-nombre varchar(100)
-)
-go
+----------------------------------------------------------------------------
 
 --PROCEDIMIENTO MOSTRAR TABLA FUNCIONALIDAD --> spMostrar_Funcionalidades --
 CREATE PROC spMostrar_Funcionalidades
@@ -173,6 +325,8 @@ as
 select * from Funcionalidad
 order by id_funcionalidad
 go
+
+----------------------------------------------------------------------------------
 
 --PROCEDIMIENTO INSERTAR ROL --> spinsertar_rol --
 CREATE PROC spinsertar_rol
@@ -182,6 +336,8 @@ CREATE PROC spinsertar_rol
 )
 as
 
+begin
+
 insert into Rol(nombre,habilitado)
 values(@nombre,@habilitado)
 
@@ -189,66 +345,23 @@ declare @id_aux int
 select @id_aux = id_rol from Rol where @nombre = nombre 
 
 return(@id_aux)
+
+end
 go
 
---PROCEDIMIENTO CREAR TABLA ROL --
-CREATE TABLE Rol
-(id_rol int PRIMARY KEY not null identity,
-nombre varchar(100),
-habilitado binary
+----------------------------------------------------------------------------------------
+
+--Crear insertar rol-funcionalidad en tabla rol_funcionalidad--
+CREATE PROC spinsertar_rol_funcionalidad
+(@id_rol int,
+@id_funcionalidad int
 )
+
+as
+
+insert into rol_funcionalidad(id_rol,id_funcionalidad)
+values(@id_rol,@id_funcionalidad)
+
 go
 
---ELIMINAR PROCEDIMIENTO spinsertar_rol--
-DROP PROC spinsertar_rol;
-go
-
---ELIMINAR TABLA ROL--
-drop table Rol;
-go
-
---ELIMINAR TABLA FUNCIONALIDAD--
-drop table Funcionalidad;
-go
-
---ELIMINAR TABLA AERONAVE--
-drop table Aeronave;
-go
-
---ELIMINAR PROCEDIMIENTO INSERTAR--
-drop proc spinsertar_aeronave;
-go
-
---ELIMINAR PROCEDIMIENTO MOSTRAR--
-drop proc spmostrar_aeronave;
-go
-
---ELIMINAR PROCEDIMIENTO BUSCAR NUMERO--
-drop proc spbuscarNombre_aeronave;
-go
-
---ELIMINAR PROCEDIMIENTO BUSCAR FABRICANTE--
-drop proc spbuscarFabricante_aeronave;
-go
-
---ELIMINAR PROCEDIMIENTO speditar_aeronave --
-DROP PROC speditar_aeronave
-go
-
---ELIMINAR PROCEDIMIENTO speliminar_aeronave--
-DROP PROC speliminar_aeronave
-go
-
---ELIMINAR PROCEDIMIENTO --
-DROP PROC spdardebaja_aerolinea
-go
-
-alter table Aeronave alter column FechaDeFueraDeServicio date
-
-alter table Aeronave alter column FechaDeReinicioDeReinicioDeServicio date
-
-alter table Aeronave alter column FechaDeBajaDefinitiva date
-
-select * from Rol
-
-exec spinsertar_rol 4,'nico',4 
+-------------------------------------------------------------------------------------------
