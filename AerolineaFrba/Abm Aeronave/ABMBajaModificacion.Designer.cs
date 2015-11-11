106f2e047304f41fe5d428ea613316ed4f288087
Messa Nicolas <nnico3691@gmail.com>
2015-11-10 23:30:16 -0300

    arreglo varios

--- AerolineaFrba/Abm Aeronave/ABMBajaModificacion.Designer.cs
+++ AerolineaFrba/Abm Aeronave/ABMBajaModificacion.Designer.cs
@@ -216,6 +216,7 @@
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Mantenimiento";
             this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
             // 
             // groupBox1
             // 
